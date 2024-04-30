76f6a1657e0010f3a601b6cad2ee76c5a38f2535
emmalum <emmalum20@gmail.com>
2024-04-30 13:09:59 +1000

    Initial Commit

---
+++ Travel/TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelApp.swift
+//  Travel
+//
+//  Created by Emma Lum on 30/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
