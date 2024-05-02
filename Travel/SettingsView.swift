8e28791726a4001c9b410343dd0c9c1732ffa2d7
Utsjoshua <Joshua.E.Isaraela@student.uts.edu.au>
2024-05-02 14:07:35 +1000

    Starting the settings view

---
+++ Travel/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Travel
+//
+//  Created by Joshua Isaraela on 2/5/2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
